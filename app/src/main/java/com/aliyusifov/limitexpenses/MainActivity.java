fa47b44bf8d7a25e0d330e11ff65fd0358c97e02
Ali <ali.yusifli0011@gmail.com>
2020-10-11 13:33:43 +0800

    Limit Expenses App

--- app/src/main/java/com/aliyusifov/limitexpenses/MainActivity.java
+++ app/src/main/java/com/aliyusifov/limitexpenses/MainActivity.java
@@ -1,13 +1,60 @@
 package com.aliyusifov.limitexpenses;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    Button setTargetButton;
+    TextView dateView;
+    EditText amountEditText;
+    String prevStarted = "prevStarted";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTargetButton = findViewById(R.id.buttonTarget);
+        dateView = findViewById(R.id.dateTextView);
+        amountEditText  = findViewById(R.id.numberEditText);
+        setDate(dateView);
     }
+    //Setting date for current date
+    public void setDate (TextView view){
+        Date today = Calendar.getInstance().getTime();//getting date
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");//formating according to my need
+        String date = formatter.format(today);
+        view.setText(date);
+        SharedPreferences sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        if (!sharedpreferences.getBoolean(prevStarted, false)) {
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putBoolean(prevStarted, Boolean.TRUE);
+            editor.apply();
+        } else {
+            finish();
+        }
+    }
+
+    //Set target for amount of money
+    public void setTarget(View view) {
+        String amount = amountEditText.getText().toString();
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        intent.putExtra("Amount", amount);
+        startActivity(intent);
+        finish();
+    }
+
 }
--- app/src/main/java/com/aliyusifov/limitexpenses/MainActivity2.java
+++ app/src/main/java/com/aliyusifov/limitexpenses/MainActivity2.java
@@ -1,13 +1,34 @@
 package com.aliyusifov.limitexpenses;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    TextView currentAmountTextView;
+    EditText editTextAmountExtract;
+    Button buttonSubtract;
+    Button buttonReset;
+    Button buttonEditGoal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        currentAmountTextView = findViewById(R.id.amountTextView);
+        editTextAmountExtract  = findViewById(R.id.editAmountExtract);
+        buttonSubtract = findViewById(R.id.buttonSubtract);
+        buttonReset = findViewById(R.id.buttonReset);
+        buttonEditGoal = findViewById(R.id.buttonEditTarget);
+    }
+
+    public void editGoal(View view) {
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(intent);
     }
 }
